97263b6e72caae3bcaa9ef44bfd73e04f9daff95
Tom Lee <thomas.j.lee@gmail.com>
2013-09-05 00:08:28 -0400

    added support for multiple predictions

--- stephmeter.py
+++ stephmeter.py
@@ -8,22 +8,23 @@ from settings import *
 
 def main():
 	l = led.LED(SERIAL_DEVICE, SERIAL_SPEED)
-	p = pwm_calibrate.PWMCalibrator()
+	p = pwm_calibrate.PWMCalibrator(smoothing=True)
 	p.load()
 	p_range = p.get_range()
 	nb = nextbus.NextbusPredictor(NEXTBUS_ROUTES)
 
 	while True:
 		nb.refresh_if_necessary()
-		(route, minutes) = nb.get_closest_arrival()
-		minutes = min(max(minutes, p_range[0]), p_range[1])
-		if int(route)==43:
-			l.set(0, 255, 0)
-		elif int(route)==42:
-			l.set(0, 0, 255)
-		p.setPWM(minutes)
+		predictions = (nb.get_nth_closest_arrival(0), nb.get_nth_closest_arrival(1))
+		for (route, minutes) in predictions:
+			minutes = min(max(minutes, p_range[0]), p_range[1])
+			if int(route)==43:
+				l.set(0, 100, 0)
+			elif int(route)==42:
+				l.set(0, 0, 100)
+			p.setPWM(minutes)
+			time.sleep(3)
 
-		time.sleep(0.1)
 
 if __name__ == '__main__':
 	main()
