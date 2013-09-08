906a6e509eab9a7e4eebd3b3f8c746c9a129cceb
sbma44 <thomas.j.lee@gmail.com>
2013-09-08 16:45:30 -0400

    sample init file; variable number of buses displayed based on criteria

--- stephmeter.py
+++ stephmeter.py
@@ -4,6 +4,7 @@ import nextbus
 import pwm_calibrate
 import led
 import time
+import datetime
 from settings import *
 
 def main():
@@ -15,13 +16,26 @@ def main():
 
 	while True:
 		nb.refresh_if_necessary()
+
+		# determine the number of predictions to display based on time of day and interval between
+		num_predictions_to_display = 1
 		predictions = (nb.get_nth_closest_arrival(0), nb.get_nth_closest_arrival(1))
-		for (route, minutes) in predictions:
+		if (predictions[0][1] is not None) and (predictions[1][1] is not None): # do we have two valid predictions?
+			if predictions[0][1]<30: # is the next bus less than 30m away?
+				if predictions[1][1]<p_range[1]: # does the second one fit on the meter?
+					num_predictions_to_display = 2
+
+
+		for (route, minutes) in predictions[:num_predictions_to_display]:
+			
+			if minutes is None:
+				minutes = p_range[1]
+
 			minutes = min(max(minutes, p_range[0]), p_range[1])
 			if int(route)==43:
-				l.set(0, 100, 0)
+				l.set(50, 100, 50)
 			elif int(route)==42:
-				l.set(0, 0, 100)
+				l.set(50, 50, 100)
 			p.setPWM(minutes)
 			time.sleep(3)
 
