2a218fbf34a8f410fd640e3c0dc844b864e80230
Tom Lee <thomas.j.lee@gmail.com>
2014-02-13 21:10:04 -0500

    correcting indentation problems; bad variable name after settings.py refactor; problem with debug mode

--- stephmeter.py
+++ stephmeter.py
@@ -15,37 +15,44 @@ def on():
 	if DEBUG:
 		print 'ON'
 	else:
-        wiringpi.digitalWrite(6, wiringpi.HIGH)
+        	wiringpi.digitalWrite(6, wiringpi.HIGH)
 
 def off():
 	if DEBUG:
 		print 'OFF'
-    else:
-    	wiringpi.digitalWrite(6, wiringpi.LOW)
+   	else:
+    		wiringpi.digitalWrite(6, wiringpi.LOW)
 
 def setup():
 	if not DEBUG:
 		wiringpi.wiringPiSetup()
-        wiringpi.pinMode(6, wiringpi.OUTPUT)	
+	        wiringpi.pinMode(6, wiringpi.OUTPUT)	
 
 def main():
 	if DEBUG:
 		print 'Entering debug mode...'
 
-	nb = nextbus.NextbusPredictor(NEXTBUS_ROUTES)
+	nb = nextbus.NextbusPredictor(NEXTBUS_URLS)
 
 	while True:
 		nb.refresh_if_necessary()
 
 		appropriate_route = None
-		for route in NEXTBUS_ROUTES.keys():
+		for route in NEXTBUS_URLS.keys():
 			if datetime.datetime.now().hour in NEXTBUS_HOURS.get(route, []):
 				appropriate_route = route
+
+
+		if DEBUG:
+			print 'found actively monitored route: %s' % appropriate_route
 
 		if appropriate_route is None:
 			continue
 
 		predictions = (nb.get_nth_closest_arrival(n=0, route=appropriate_route),)
+
+		if DEBUG:
+			print 'predictions: %s' % str(predictions)
 
 		light_should_be_on = False
 		for pred in predictions:
@@ -70,13 +77,14 @@ def main():
 if __name__ == '__main__':
 	setup()
 	off()
+	main()
 
-	try:
-		main()
-	except Exception, e:
-		f = open('/var/log/stephmeter-crash.log', 'a')
-		f.write(str(e))
-		f.close()
+	#try:
+	#	main()
+	#except Exception, e:
+	#	f = open('/var/log/stephmeter-crash.log', 'a')
+	#	f.write(str(e))
+	#	f.close()
 
-		os.system('echo "%s" | mail -s "YELLOWLIGHT CRASH LOG" thomas.j.lee@gmail.com' % str(e))
+	#	os.system('echo "%s" | mail -s "YELLOWLIGHT CRASH LOG" thomas.j.lee@gmail.com' % str(e))
 
