b7913160f4a29284b159efe69caf26d5aac1a49b
Tom Lee <thomas.j.lee@gmail.com>
2015-01-02 15:33:41 -0500

    fixing problems in install script, tabs in main script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt-get update -y
 sudo apt-get install -y supervisor git python-setuptools
 sudo mkdir -p /usr/local/etc
 sudo pip install -e git+https://github.com/sbma44/nextbus.git#egg=nextbus
-sudo git clone git@github.com:sbma44/yellowlight.git /usr/local/etc/yellowlight
+sudo git clone https://github.com/sbma44/yellowlight.git /usr/local/etc/yellowlight
 sudo pip install -r /usr/local/etc/yellowlight/requirements.txt
 sudo cp /usr/local/etc/yellowlight/yellowlight.conf /etc/supervisor/conf.d/yellowlight.conf
-sudo /usr/sbin/service restart supervisor
+sudo /usr/sbin/service supervisor restart
